0253d5c23d8943a5438a8662f63d8c66fb93fb01
Alexandre Assouad <assouad@kairosfire.com>
2018-11-06 23:29:44 +0100

    improved code structure

--- internal/dnsupdater/dnsupdater.go
+++ internal/dnsupdater/dnsupdater.go
@@ -2,6 +2,6 @@ package dnsupdater
 
 import "github.com/t0k4rt/dynamic-dns/internal/ipaddressprovider"
 
-type IPProvider interface {
+type DNSProvider interface {
 	UpdateDNS(ip *ipaddressprovider.ProvidedIP) error
 }
