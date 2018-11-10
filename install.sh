b00f0057e56340fec65da02b6d76e61aa678f837
Alexandre Assouad <assouad@kairosfire.com>
2018-11-10 22:21:16 +0100

    fixed install script and systemd service conf

--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@
 latest_tag=$(git describe --abbrev=0 --tags)
 os=$(uname)
 arch=$(uname -m)
-filename="dynamic-dns_{$latest_tag}_{$os}_{$arch}.tar.gz"
+filename="dynamic-dns_${latest_tag}_${os}_${arch}.tar.gz"
 
 wget "https://github.com/t0k4rt/dynamic-dns/releases/download/${latest_tag}/${filename}"
 tar -xvf $filename
 chmod +x dynamic-dns
 mv dynamic-dns /usr/local/bin/
 
-mkdir /etc/dynamicdns
+mkdir -p /etc/dynamicdns
 cp ./etc/dynamicdns.toml /etc/dynamicdns/dynamicdns.toml
 cp ./etc/dynamicdns_env.conf /etc/dynamicdns/dynamicdns_env.conf
+chown -R www-data:www-data /etc/dynamicdns
+
 cp ./scripts/dynamicdns.service /etc/systemd/system/dynamicdns.service
