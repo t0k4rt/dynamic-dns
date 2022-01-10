604b02acc788ad6b8e8c63388513d4e4f56a16bf
Alexandre Assouad <assouad@kairosfire.com>
2022-01-10 21:02:10 +0100

    improve install script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dynamic DNS
+
+Dynamix dns is a dyndns alternative for your homeserver. It's a standalone bainry that fetch your external ip and set your dns server according to it.
+
+
+## Install dynamic dns
+
+
+
--- install.sh
+++ install.sh
@@ -1,23 +1,58 @@
 #!/bin/bash
-
+set -e
 # for ubuntu
-latest_tag=$(git describe --abbrev=0 --tags)
-os=$(uname)
-arch=$(uname -m)
-filename="dynamic-dns_${latest_tag}_${os}_${arch}.tar.gz"
+VERSION=1.0.0
+OS=$(uname)
+ARCH=$(uname -m)
+USER=dynamicdns
 
-wget "https://github.com/t0k4rt/dynamic-dns/releases/download/${latest_tag}/${filename}"
+
+while getopts ":v:" opt; do
+  case $opt in
+    v)
+      echo "-v was triggered, Parameter: $OPTARG" >&2
+      VERSION=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "Install DynamiycDNS version $VERSION"
+
+if id "$USER" &>/dev/null; then
+  echo 'User already exists'
+else
+  if [[ "$OS" == "Linux" ]]; then
+    useradd -r -s /bin/false "$USER"
+  fi
+fi
+
+filename="dynamic-dns_${VERSION}_${OS}_${ARCH}.tar.gz"
+
+SERVICE_FILE_URL="https://raw.githubusercontent.com/t0k4rt/dynamic-dns/${VERSION}/scripts/dynamicdns.service"
+DEFAULT_CONFIG_FILE_URL="https://raw.githubusercontent.com/t0k4rt/dynamic-dns/${VERSION}/etc/dynamicdns.toml"
+DEFAULT_ENV_FILE_URL="https://raw.githubusercontent.com/t0k4rt/dynamic-dns/${VERSION}/etc/dynamicdns_env.conf"
+
+wget "https://github.com/t0k4rt/dynamic-dns/releases/download/${VERSION}/${filename}"
 tar -xvf $filename
 chmod +x dynamic-dns
 mv dynamic-dns /usr/local/bin/
 
 mkdir -p /etc/dynamicdns
-cp ./etc/dynamicdns.toml /etc/dynamicdns/dynamicdns.toml
-cp ./etc/dynamicdns_env.conf /etc/dynamicdns/dynamicdns_env.conf
-chown -R www-data:www-data /etc/dynamicdns
+curl -L "$DEFAULT_CONFIG_FILE_URL" -o /etc/dynamicdns/dynamicdns.toml
+curl -L "$DEFAULT_ENV_FILE_URL" -o /etc/dynamicdns/dynamicdns_env.conf
+chown -R "$USER:$USER" /etc/dynamicdns
 
 touch /var/log/dynamicdns.log
-chown www-data:www-data /var/log/dynamicdns.log
+chown "$USER:$USER" /var/log/dynamicdns.log
 
-cp ./scripts/dynamicdns.service /etc/systemd/system/dynamicdns.service
+curl -L "$SERVICE_FILE_URL" -o /etc/systemd/system/dynamicdns.service
+
 rm $filename
